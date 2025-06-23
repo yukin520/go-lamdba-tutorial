fceb8e00b5a20728032e2dd357afb6fcf2d2d863
Yuki Noda <yuki.handball@icloud.com>
2025-06-23 23:17:45 +0900

    tidy hendler function.

--- app/user/handler/main.go
+++ app/user/handler/main.go
@@ -46,205 +46,178 @@ func LamdaHandler(context context.Context, request events.APIGatewayV2HTTPReques
 	log.Println("Init lamdba handler.")
 	var response events.APIGatewayProxyResponse
 
-	if string(request.RequestContext.HTTP.Method) == "GET" {
-		// List Items.
-		if request.RequestContext.HTTP.Path == "/" {
-			todos, err := todoUsecase.ListTodo(context)
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to fech todo list.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-			} else {
-				response, _ = infra.APIResponse(200, todos)
-			}
-			return response, nil
-		}
-		// Get Item by ID.
-		if request.RequestContext.HTTP.Path == "/item" {
-			todoItemId, err := strconv.Atoi(request.QueryStringParameters["id"])
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to fech todo item.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-				return response, nil
-			}
-
-			todo, err := todoUsecase.GetTodo(context, uint(todoItemId))
-			if errors.Is(err, domain.ErrNotFound) {
-				body := struct{ Msg string }{
-					Msg: "\"todo item not found.\"",
-				}
-				response, _ = infra.APIResponse(404, body)
-				return response, nil
-			}
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to fech todo item.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-				return response, nil
-			}
-			response, _ = infra.APIResponse(200, todo)
-			return response, nil
-		}
-	}
-
-	if string(request.RequestContext.HTTP.Method) == "POST" {
-		// Create Todo Item.
-		if request.RequestContext.HTTP.Path == "/item" {
-			var params todoRequestParams
-			if len(request.Body) <= 0 {
-				return infra.APIResponse(400, domain.ErrInvalidParameters)
-			}
-			err := json.Unmarshal([]byte(request.Body), &params)
-			if err != nil {
-				log.Printf("Unmarshal error: %v\n", err)
-				return infra.APIResponse(400, domain.ErrInvalidParameters)
-			}
-
-			loc, _ := time.LoadLocation("Asia/Tokyo")
-			todoData := domain.ToDo{
-				Id:          params.Id,
-				Name:        params.Name,
-				Description: params.Description,
-				Completed:   params.Completed,
-				CreatedAt:   time.Now().In(loc),
-				UpdatedAt:   time.Now().In(loc),
-			}
-
-			createdItemId, err := todoUsecase.CreateTodo(context, &todoData)
-			if errors.Is(err, domain.ErrAlreadyExists) {
-				body := struct{ Msg string }{
-					Msg: "\"todo item is already exists.\"",
-				}
-				response, _ = infra.APIResponse(400, body)
-				return response, nil
-			}
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to create todo item.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-				return response, nil
-			}
-
-			body := struct {
-				Msg string
-				Id  uint
-			}{
-				Msg: "\"todo item is created.\"",
-				Id:  createdItemId,
-			}
-			response, _ = infra.APIResponse(200, body)
-			return response, nil
-		}
-
-		// Incorrect path.
-		body := struct{ Msg string }{
-			Msg: "\"not found.\"",
-		}
-		response, _ = infra.APIResponse(404, body)
-		return response, nil
-	}
-
-	if string(request.RequestContext.HTTP.Method) == "PUT" {
-		// Update Todo Item.
-		if request.RequestContext.HTTP.Path == "/item" {
-			var params todoRequestParams
-			if len(request.Body) <= 0 {
-				return infra.APIResponse(400, domain.ErrInvalidParameters)
-			}
-			err := json.Unmarshal([]byte(request.Body), &params)
-			if err != nil {
-				log.Printf("Unmarshal error: %v\n", err)
-				return infra.APIResponse(400, domain.ErrInvalidParameters)
-			}
-
-			loc, _ := time.LoadLocation("Asia/Tokyo")
-			todoData := domain.ToDo{
-				Id:          params.Id,
-				Name:        params.Name,
-				Description: params.Description,
-				Completed:   params.Completed,
-				CreatedAt:   time.Now().In(loc),
-				UpdatedAt:   time.Now().In(loc),
-			}
-
-			updatedTodoItem, err := todoUsecase.UpdateTodo(context, &todoData)
-			if errors.Is(err, domain.ErrNotFound) {
-				body := struct{ Msg string }{
-					Msg: "\"todo item is not found.\"",
-				}
-				response, _ = infra.APIResponse(400, body)
-				return response, nil
-			}
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to update todo item.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-				return response, nil
-			}
-			response, _ = infra.APIResponse(200, updatedTodoItem)
-			return response, nil
-		}
-
-		// Incorrect path.
-		body := struct{ Msg string }{
-			Msg: "\"not found.\"",
-		}
-		response, _ = infra.APIResponse(404, body)
-		return response, nil
-	}
-
-	if string(request.RequestContext.HTTP.Method) == "DELETE" {
-		// Delete Todo Item.
-		if strings.HasPrefix(request.RawPath, "/item/") {
-			todoItemId, err := strconv.Atoi(request.PathParameters["id"])
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to delete todo item.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-				return response, nil
-			}
-
-			err = todoUsecase.DeleteTodo(context, uint(todoItemId))
-			if errors.Is(err, domain.ErrNotFound) {
-				body := struct{ Msg string }{
-					Msg: "\"todo item is not found.\"",
-				}
-				response, _ = infra.APIResponse(400, body)
-				return response, nil
-			}
-			if err != nil {
-				body := struct{ Msg string }{
-					Msg: "\"faild to delete todo item.\"",
-				}
-				response, _ = infra.APIResponse(500, body)
-				return response, nil
-			}
-
+	// List Todo Items.
+	if string(request.RequestContext.HTTP.Method) == "GET" && request.RequestContext.HTTP.Path == "/" {
+		todos, err := todoUsecase.ListTodo(context)
+		if err != nil {
 			body := struct{ Msg string }{
-				Msg: "\"todo item is delted sccessfully.\"",
+				Msg: "\"faild to fech todo list.\"",
 			}
-			response, _ = infra.APIResponse(200, body)
-			return response, nil
+			response, _ = infra.APIResponse(500, body)
+		} else {
+			response, _ = infra.APIResponse(200, todos)
 		}
-
-		// Incorrect path.
-		body := struct{ Msg string }{
-			Msg: "\"not found.\"",
-		}
-		response, _ = infra.APIResponse(404, body)
 		return response, nil
 	}
 
-	body := struct{ Msg string }{
-		Msg: "\"Http method is not supported.\"",
+	// Get Todo Item by ID.
+	if string(request.RequestContext.HTTP.Method) == "GET" && request.RequestContext.HTTP.Path == "/item" {
+		todoItemId, err := strconv.Atoi(request.QueryStringParameters["id"])
+		if err != nil {
+			body := struct{ Msg string }{
+				Msg: "\"faild to fech todo item.\"",
+			}
+			response, _ = infra.APIResponse(500, body)
+			return response, nil
+		}
+
+		todo, err := todoUsecase.GetTodo(context, uint(todoItemId))
+		if errors.Is(err, domain.ErrNotFound) {
+			body := struct{ Msg string }{
+				Msg: "\"todo item not found.\"",
+			}
+			response, _ = infra.APIResponse(404, body)
+			return response, nil
+		}
+		if err != nil {
+			body := struct{ Msg string }{
+				Msg: "\"faild to fech todo item.\"",
+			}
+			response, _ = infra.APIResponse(500, body)
+			return response, nil
+		}
+		response, _ = infra.APIResponse(200, todo)
+		return response, nil
 	}
-	response, _ = infra.APIResponse(500, body)
-	return response, errors.New("http method is not supported")
+
+	// Create Todo Item.
+	if string(request.RequestContext.HTTP.Method) == "POST" && request.RequestContext.HTTP.Path == "/item" {
+		var params todoRequestParams
+		if len(request.Body) <= 0 {
+			return infra.APIResponse(400, domain.ErrInvalidParameters)
+		}
+		err := json.Unmarshal([]byte(request.Body), &params)
+		if err != nil {
+			log.Printf("Unmarshal error: %v\n", err)
+			return infra.APIResponse(400, domain.ErrInvalidParameters)
+		}
+
+		loc, _ := time.LoadLocation("Asia/Tokyo")
+		todoData := domain.ToDo{
+			Id:          params.Id,
+			Name:        params.Name,
+			Description: params.Description,
+			Completed:   params.Completed,
+			CreatedAt:   time.Now().In(loc),
+			UpdatedAt:   time.Now().In(loc),
+		}
+
+		createdItemId, err := todoUsecase.CreateTodo(context, &todoData)
+		if errors.Is(err, domain.ErrAlreadyExists) {
+			body := struct{ Msg string }{
+				Msg: "\"todo item is already exists.\"",
+			}
+			response, _ = infra.APIResponse(400, body)
+			return response, nil
+		}
+		if err != nil {
+			body := struct{ Msg string }{
+				Msg: "\"faild to create todo item.\"",
+			}
+			response, _ = infra.APIResponse(500, body)
+			return response, nil
+		}
+
+		body := struct {
+			Msg string
+			Id  uint
+		}{
+			Msg: "\"todo item is created.\"",
+			Id:  createdItemId,
+		}
+		response, _ = infra.APIResponse(200, body)
+		return response, nil
+	}
+
+	// Update Todo Item.
+	if string(request.RequestContext.HTTP.Method) == "PUT" && request.RequestContext.HTTP.Path == "/item" {
+		var params todoRequestParams
+		if len(request.Body) <= 0 {
+			return infra.APIResponse(400, domain.ErrInvalidParameters)
+		}
+		err := json.Unmarshal([]byte(request.Body), &params)
+		if err != nil {
+			log.Printf("Unmarshal error: %v\n", err)
+			return infra.APIResponse(400, domain.ErrInvalidParameters)
+		}
+
+		loc, _ := time.LoadLocation("Asia/Tokyo")
+		todoData := domain.ToDo{
+			Id:          params.Id,
+			Name:        params.Name,
+			Description: params.Description,
+			Completed:   params.Completed,
+			CreatedAt:   time.Now().In(loc),
+			UpdatedAt:   time.Now().In(loc),
+		}
+
+		updatedTodoItem, err := todoUsecase.UpdateTodo(context, &todoData)
+		if errors.Is(err, domain.ErrNotFound) {
+			body := struct{ Msg string }{
+				Msg: "\"todo item is not found.\"",
+			}
+			response, _ = infra.APIResponse(400, body)
+			return response, nil
+		}
+		if err != nil {
+			body := struct{ Msg string }{
+				Msg: "\"faild to update todo item.\"",
+			}
+			response, _ = infra.APIResponse(500, body)
+			return response, nil
+		}
+		response, _ = infra.APIResponse(200, updatedTodoItem)
+		return response, nil
+	}
+
+	// Delete Todo Item.
+	if string(request.RequestContext.HTTP.Method) == "DELETE" && strings.HasPrefix(request.RawPath, "/item/") {
+		todoItemId, err := strconv.Atoi(request.PathParameters["id"])
+		if err != nil {
+			body := struct{ Msg string }{
+				Msg: "\"faild to delete todo item.\"",
+			}
+			response, _ = infra.APIResponse(500, body)
+			return response, nil
+		}
+
+		err = todoUsecase.DeleteTodo(context, uint(todoItemId))
+		if errors.Is(err, domain.ErrNotFound) {
+			body := struct{ Msg string }{
+				Msg: "\"todo item is not found.\"",
+			}
+			response, _ = infra.APIResponse(400, body)
+			return response, nil
+		}
+		if err != nil {
+			body := struct{ Msg string }{
+				Msg: "\"faild to delete todo item.\"",
+			}
+			response, _ = infra.APIResponse(500, body)
+			return response, nil
+		}
+
+		body := struct{ Msg string }{
+			Msg: "\"todo item is delted sccessfully.\"",
+		}
+		response, _ = infra.APIResponse(200, body)
+		return response, nil
+	}
+
+	// Other
+	body := struct{ Msg string }{
+		Msg: "\"Http method or request path is not supported.\"",
+	}
+	response, _ = infra.APIResponse(400, body)
+	return response, errors.New("http method  or request path is not supported")
 }
